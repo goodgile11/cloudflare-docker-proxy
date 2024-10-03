e32d94f0247e1aa923a5ac16b485819c0828a2df
goodgile11 <goodgile11@gmail.com>
2024-10-03 15:03:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
   "ecr.mmcc.us.kg": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mmcc.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
